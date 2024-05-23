ddc60487c44734ac3b5c49e7686cb5e0645240a4
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-05-23 09:38:22 -0300

    atividade 45: função retonando Olá mundo. Atividade 46: Função retornando a soma de dois argumentos

---
+++ Exercício 45/main.js
@@ -0,0 +1,10 @@
+// Exercício 45: Escreva uma função que imprima "Olá, Mundo!".
+
+// Descrição: Neste exercício, você deve escrever uma função simples que não recebe argumentos e,
+// quando chamada, imprime a frase "Olá, Mundo!".
+
+function relou() {
+  console.log("Relou ord!");
+}
+
+relou();
---
+++ Exercício 46/main.js
@@ -0,0 +1,9 @@
+// Exercício 46: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
+
+// Descrição: Você deve escrever uma função que receba dois números como argumentos e retorne a soma desses números.
+
+function soma(a, b) {
+  return a + b;
+}
+
+console.log(soma(5, 7));
