13084769a41386f538ea085303919c695cb30af3
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-06-04 16:18:44 -0300

    atividade 53: retornar true se numero for primo

---
+++ Exercício 53/main.js
@@ -0,0 +1,5 @@
+// Exercício 52: Escreva uma função que calcule o n-ésimo número de Fibonacci.
+
+// Descrição: A sequência de Fibonacci começa com os números 0 e 1, e cada número subsequente 
+// é a soma dos dois números anteriores. Neste exercício, você deve escrever uma função que retorne 
+// o n-ésimo número da sequência de Fibonacci.
